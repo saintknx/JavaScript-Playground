e42453857cb49fd3213684f190d22083e7382881
Kauã Sousa Linux <kauasantos2014s10@gmail.com>
2026-04-30 12:52:18 -0300

    Variáveis

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-alert('Hello, World!');
+// var message = "Hello, World!"; Não recomendado, variável de escopo global
+const message = "Hello, World!"; // Variável de escopo local e imutável  
+// let message = "Hello, World!"; Variável de escopo local e mutável
+// message = "Hello, Universe!"; // Isso causará um erro, pois 'message' é uma constante
+
+alert(message);
